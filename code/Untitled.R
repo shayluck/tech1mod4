ae53bcdea25d98b7f3caff27c760a2dc71817791
shayluck <shay.luck@my.jcu.edu.au>
2024-05-16 11:55:15 +1000

    j

--- code/Untitled.R
+++ code/Untitled.R
@@ -35,4 +35,51 @@ ggplot(data = mpg) +
 #can change color name, size point, shape of point
 # + has to go at end of line, not at beginning of next line
 
+
+#show subset of data by plotting 1 variable
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy)) + 
+  facet_wrap(~ class, nrow = 2)
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy)) + 
+  facet_grid(drv ~ cyl)
              
+###....................###
+#Fitting simple lines
+
+#geom points to create graph of data points
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy))
+
+#display points as smooth line
+ggplot(data = mpg) + 
+  geom_smooth(mapping = aes(x = displ, y = hwy))
+
+#geom: plot uses this to represent data, can change function, line type
+
+ggplot(data = mpg) + 
+  #  geom_point(mapping = aes(x = displ, y = hwy)) # points horrible 
+  geom_smooth(mapping = aes(x = displ, y = hwy)) # try smooth line
+
+ggplot(data = mpg) + 
+  geom_smooth(mapping = aes(x = displ, y = hwy, linetype = drv))
+
+
+#can be used to vary plots by discrete variable
+ggplot(data = mpg) +
+  geom_smooth(mapping = aes(x = displ, y = hwy, group = drv))
+
+
+#change colors based on category of drv
+ggplot(data = mpg) +
+  geom_smooth(
+    mapping = aes(x = displ, y = hwy, color = drv),
+    show.legend = FALSE,
+  )
+
+#two lines of code, one for points and one for line on same graph
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy)) +
+  geom_smooth(mapping = aes(x = displ, y = hwy))
+
