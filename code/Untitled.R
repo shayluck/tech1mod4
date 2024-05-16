653db4e67583df8320471f338f1dd94d7afb6660
shayluck <shay.luck@my.jcu.edu.au>
2024-05-16 11:44:38 +1000

    yea

--- code/Untitled.R
+++ code/Untitled.R
@@ -3,3 +3,36 @@
 #hi
 
 #Edit → Save → Stage → Commit → Pull → Push
+
+install.packages("tidyverse")
+library(tidyverse)
+
+mpg
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy))
+
+ggplot() 
+
+#change point color by class
+ggplot(data = mpg)
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, colour = class))
+
+#change point size by class
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, size = class))
+#> Warning: Using size for a discrete variable is not advised. 
+
+#dont use ggplot for discrete data
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, alpha = class))
+
+#change points by shape
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, shape = class))
+
+#change appearance manually
+#can change color name, size point, shape of point
+# + has to go at end of line, not at beginning of next line
+
+             
